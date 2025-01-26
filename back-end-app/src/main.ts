376e075b2f2fc17dabbafc8bdf183a5da69b93e3
Alex <aletar89@gmail.com>
2025-01-26 16:04:51 +0200

    Fix backend server configuration

--- back-end-app/src/main.ts
+++ back-end-app/src/main.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 
-const host = process.env.HOST ?? 'localhost';
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 const app = express();
@@ -13,6 +12,6 @@ app.get('/inventory', (req, res) => {
   res.send({ message: 'This will return the inventory data' });
 });
 
-app.listen(port, host, () => {
-  console.log(`[ ready ] http://${host}:${port}`);
+app.listen(port, () => {
+  console.log(`[ ready ] http://localhost:${port}`);
 });
