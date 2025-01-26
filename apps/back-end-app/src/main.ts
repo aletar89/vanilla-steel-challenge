80789377c9485775455cef4c18afc7c8dc0ba030
Alex <aletar89@gmail.com>
2025-01-26 16:17:08 +0200

    Move into apps

--- apps/back-end-app/src/main.ts
+++ apps/back-end-app/src/main.ts
@@ -5,11 +5,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello, I am the back-end API Alex edited' });
-});
-
-app.get('/inventory', (req, res) => {
-  res.send({ message: 'This will return the inventory data' });
+  res.send({ message: 'Hello API' });
 });
 
 app.listen(port, () => {
