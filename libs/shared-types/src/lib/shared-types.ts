87228c8b7c97cd2e2de6efddbad9ff46041096d7
Alex <aletar89@gmail.com>
2025-01-27 21:17:09 +0200

    use pg with kysley

--- libs/shared-types/src/lib/shared-types.ts
+++ libs/shared-types/src/lib/shared-types.ts
@@ -1,6 +1,38 @@
 export function sharedTypes(): string {
   return 'shared-types';
 }
+
+import { ColumnType } from 'kysely';
+
+export interface Database {
+  inventory: InventoryTable;
+}
+
+export interface InventoryTable {
+  id: ColumnType<number>;
+  productNumber: string;
+  material: string;
+  form: string;
+  choice: string;
+  grade: string;
+  finish: string | null;
+  surface: string | null;
+  quantity: number;
+  weight: number;
+  length: number | null;
+  width: number | null;
+  height: number | null;
+  thickness: number | null;
+  outerDiameter: number | null;
+  wallThickness: number | null;
+  webThickness: number | null;
+  flangeThickness: number | null;
+  certificates: string | null;
+  location: string | null;
+  createdAt: ColumnType<Date>;
+  updatedAt: ColumnType<Date>;
+}
+
 export interface DataItem {
   id: number;
   name: string;
